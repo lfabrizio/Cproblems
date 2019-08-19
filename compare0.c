a7c5491a0d17b9c3489d3f624ded2c9bb5b4be3a
lfabrizio <48811036+lfabrizio@users.noreply.github.com>
2019-08-19 18:41:39 -0400

    Create compare0.c

---
+++ compare0.c
@@ -0,0 +1,19 @@
+// compare0.c
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int i = get_int("i: ");
+	int j = get_int("j: ");
+
+	if (i == j)
+	{
+	printf("same\n");
+	}
+	else
+	{
+	printf("different\n);
+}
+}	
